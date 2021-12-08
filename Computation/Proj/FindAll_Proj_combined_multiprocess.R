cc19536b25b4a2f9307b58acbfce8d233ed96d4f
prachsk <prach.techa@gmail.com>
2021-12-08 20:45:44 +0100

    Update scripts

--- Computation/Proj/FindAll_Proj_combined_multiprocess.R
+++ Computation/Proj/FindAll_Proj_combined_multiprocess.R
@@ -10,11 +10,11 @@ options(future.globals.maxSize = 7000 * 1024^2)
 proj.combined <- readRDS("./RDS_obj/Proj_combined.RDS")
 
 # Run the standard workflow for visualization and clustering
-#proj.combined <- ScaleData(proj.combined, verbose = FALSE)
-#proj.combined <- RunPCA(proj.combined, npcs = 200, verbose = FALSE)
-#proj.combined <- RunUMAP(proj.combined, reduction = "pca", dims = 1:70, n.neighbors = 40, n.epochs = 500, min.dist = 0.5, spread = 0.4, negative.sample.rate = 10)
-#proj.combined <- FindNeighbors(proj.combined, reduction = "pca", dims = 1:70)
-#proj.combined <- FindClusters(proj.combined, resolution = 0.5)
+proj.combined <- ScaleData(proj.combined, verbose = FALSE)
+proj.combined <- RunPCA(proj.combined, npcs = 200, verbose = FALSE)
+proj.combined <- RunUMAP(proj.combined, reduction = "pca", dims = 1:70, n.neighbors = 40, n.epochs = 500, min.dist = 0.5, spread = 0.4, negative.sample.rate = 10)
+proj.combined <- FindNeighbors(proj.combined, reduction = "pca", dims = 1:70)
+proj.combined <- FindClusters(proj.combined, resolution = 0.5)
 
 # Set Default assay to RNA
 DefaultAssay(proj.combined) <- "RNA"
